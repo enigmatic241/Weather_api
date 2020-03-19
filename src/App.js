2c7bf13346f2ce8147398984038b29bd1505862b
enigmatic241 <raviranjan241@gmail.com>
2020-03-19 11:56:18 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -8,9 +8,9 @@ const api={
 }
 
 const dateBuilder = (d) =>{
-      let months=["January","February","March","April","May","June","July",
+    let months=["January","February","March","April","May","June","July",
     "August","September","October","November","December"];
-    let days =["Sunday","Monday","Tuesday","Wednesday"]
+    let days =["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
 
       let day= days[d.getDay()];
